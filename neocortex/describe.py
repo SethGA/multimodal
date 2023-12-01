80a0d4875980382bebc09056cf18528c8321223f
Seth Grief-Albert <sethgriefalbert@gmail.com>
2023-12-01 00:18:36 -0500

    Remove unnecessary import from describe.py

--- neocortex/describe.py
+++ neocortex/describe.py
@@ -1,13 +1,12 @@
 import time
 import os
-import cv2
 import torch
 from datetime import datetime
 from PIL import Image
+from lavis.models import load_model_and_preprocess
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-from lavis.models import load_model_and_preprocess
 model, vis_processors, _ = load_model_and_preprocess(name="blip_caption", model_type="base_coco", is_eval=True, device=device)
 
 with open("./neocortex/description.txt", "w") as clear_file:
